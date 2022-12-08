bc68678021f7330c12e266587fe3461e5f5a089e
Furji UMY <furji.ft21@mail.umy.ac.id>
2022-12-08 15:52:47 +0700

    add public method

--- Exe4_130/Program.cs
+++ Exe4_130/Program.cs
@@ -6,11 +6,18 @@ using System.Threading.Tasks;
 
 namespace Exe4_130
 {
-    class Stack
+    class Stacks
     {
         private string[] furji;
         private int top;
         private int max;
+
+        public Stacks()
+        {
+            furji = new string[49];
+            top = -1;
+            max = 49;
+        }
 
         static void Main(string[] args)
         {
